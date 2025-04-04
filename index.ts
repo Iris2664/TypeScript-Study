9ac6b1dfeca7140c48310a222297960a9bfc7cdc
HINATA Ayame <137654183+Iris2664@users.noreply.github.com>
2025-04-04 11:36:18 +0900

    studying

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
+import { createSolutionBuilderWithWatchHost } from "typescript";
+
+const file = Bun.file("output.txt");
+const source = await file.text();
+console.log(source);
+
 const now = new Date();
 
-Bun.write("output.txt", now.toString());
+
+const writer = file.writer();
+writer.write(source);
+writer.write("\n");
+writer.write(createSolutionBuilderWithWatchHost.toString());
+writer.end();
